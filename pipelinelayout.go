428aa4cf997b8d72619e54ea80f0e11a89d2e946
celer <celer@bear.local>
2020-01-09 16:53:50 -0700

    Start of push constant support

--- pipelinelayout.go
+++ pipelinelayout.go
@@ -13,6 +13,37 @@ func (p *PipelineLayout) Destroy() {
 	vk.DestroyPipelineLayout(p.Device.VKDevice, p.VKPipelineLayout, nil)
 }
 
+func (d *Device) CreatePipelineLayoutWithPushConstants(descriptorSetLayouts []*DescriptorSetLayout, pushConstants []vk.PushConstantRange) (*PipelineLayout, error) {
+	var pipelineLayoutCreateInfo = vk.PipelineLayoutCreateInfo{}
+	pipelineLayoutCreateInfo.SType = vk.StructureTypePipelineLayoutCreateInfo
+	pipelineLayoutCreateInfo.SetLayoutCount = uint32(len(descriptorSetLayouts))
+
+	l := make([]vk.DescriptorSetLayout, len(descriptorSetLayouts))
+	for i, dsl := range descriptorSetLayouts {
+		l[i] = dsl.VKDescriptorSetLayout
+	}
+
+	pipelineLayoutCreateInfo.PSetLayouts = l
+
+	pipelineLayoutCreateInfo.PushConstantRangeCount = uint32(len(pushConstants))
+	pipelineLayoutCreateInfo.PPushConstantRanges = pushConstants
+
+	var pipelineLayout vk.PipelineLayout
+
+	err := vk.Error(vk.CreatePipelineLayout(d.VKDevice, &pipelineLayoutCreateInfo, nil, &pipelineLayout))
+	if err != nil {
+		return nil, err
+	}
+
+	var ret PipelineLayout
+
+	ret.VKPipelineLayout = pipelineLayout
+	ret.Device = d
+
+	return &ret, nil
+
+}
+
 func (d *Device) CreatePipelineLayout(descriptorSetLayouts ...*DescriptorSetLayout) (*PipelineLayout, error) {
 	var pipelineLayoutCreateInfo = vk.PipelineLayoutCreateInfo{}
 	pipelineLayoutCreateInfo.SType = vk.StructureTypePipelineLayoutCreateInfo
